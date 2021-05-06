7a1c64c6367a1175efc313715e2a157803572d64
SaiSri Angajala <63279776+SaiSri44@users.noreply.github.com>
2021-05-06 08:38:11 +0530

    added the calculator ,temperature, input_temperature,screenshot functions and just commented out the hello jarvis code .

--- features.py
+++ features.py
@@ -100,4 +100,59 @@ def google_search(search) :
         webbrowser.open(search)
         speak("sir , this is what i found in the google") 
 
+def wolfram(query) :
+    import wolframalpha
+    api_key = "YQRJPU-PULT2Y3QX3"
+    requester = wolframalpha.Client(api_key) #requesting the information from the wolframalpha database
+    if "outside" in query or "surroundings" in query:
+        location  = find_location(shout=False)
+        query = query.replace("outside",location) 
         
+    else :
+       
+        requested  = requester.query(query)    
+
+    try :
+       requested = requester.query(query) #asking for the answer for our query
+       Answer = next(requested.results).text #answer might be text or videos or pictures,but we want only text so extracting only text from the answer provided by the wolframalpha, the answer may not find in the database so we included it in the try block 
+       return Answer 
+    except:
+        speak("sir, your query is not processed ,sorry for the inconvenience") 
+        return "error" 
+
+def calculator(query) :
+    query = query.replace("jarvis","")
+    query = query.replace("calculate","")
+    query = query.replace("into","*")
+    query = query.replace("minus","-")
+    query = query.replace("plus","+")
+    query = query.replace("divided by","/") 
+    query = query.replace("multiply","*") 
+    query = query.replace("power","^") 
+    print(f"{query} = " ,end=" ") 
+    result = wolfram(query) 
+    if result!="error" :
+      print(f"{result}") 
+      speak(f"the result of the calculation is {result}") 
+
+def input_calculator(query) :
+    print(f"{query} = ", end=" ") 
+    result = wolfram(query)
+    if result!= "error" :
+       print(f"{result}")
+       speak(f"the result of the calculation is {result}") 
+
+def taking_screen_shot():
+        speak("sir,please tell me the name of this screenshot file")
+        name = take_command().lower()
+        speak("Hang on for few seconds sir , I am taking the screenshot")
+        time.sleep(3)
+        img = pyautogui.screenshot() 
+        img.save(f"{name}.jpg")
+        speak(
+            "ok sir i am done with taking screenshot, it is saved in our current folder, i am ready for another command sir")
+    
+   
+# print(wolfram("days until may 24 2021"))  
+  
+# print(calculate("2 into 3 plus 4 power 2"))         
--- jarvis.py
+++ jarvis.py
@@ -1,4 +1,5 @@
 
+from features import wish
 import pyttsx3
 import speech_recognition as sr
 import sys
@@ -21,7 +22,7 @@ def take_command():
     with sr.Microphone() as source:
         print("Listening....")
         try:
-            audio = r.listen(source, timeout=10, phrase_time_limit=5)
+            audio = r.listen(source, timeout=10, phrase_time_limit=10)
         except:
             pass
     try:
@@ -44,8 +45,12 @@ def desire():
             pass
 
         elif "temperature" in query:
-            from features import weather_forecast
-            weather_forecast()
+            from features import wolfram
+            query = query.replace("jarvis", "")
+            query = query.replace("what is", "")
+            speak("sir , please be patient , fetching temperature might take time")
+            if wolfram(query) != "error":
+                speak(f" {query} is {wolfram(query)} ")
 
         elif "location" in query:
             from features import find_location
@@ -55,55 +60,74 @@ def desire():
             speak("what do you wanna seach in youtube sir")
             search = take_command()
             from features import youtube_search
-            youtube_search(search) 
+            youtube_search(search)
 
         elif "play songs on youtube" in query:
             from features import play_song_yt
             play_song_yt()
-            
-        elif "set alarm" in query :
+
+        elif "set alarm" in query:
             from features import alarm
-            alarm()     
-        
-        elif "search in google" in query or "open google" in query or "open chrome" in query :
+            alarm()
+
+        elif "search in google" in query or "open google" in query or "open chrome" in query:
             speak("what do you wanna search in google sir")
             search = take_command()
             from features import google_search
-            google_search(search) 
+            google_search(search)
 
-        elif "go to sleep" in query or "sleep" in query :
+        elif "go to sleep" in query or "sleep" in query:
             speak("ok sir, i am going to sleep you can call me at any time")
-            break 
+            break
 
-        elif "go offline" in query or "offline" in query :
-            speak("ok sir i am going offlien") 
+        elif "go offline" in query or "offline" in query:
+            speak("ok sir i am going offlien")
             speak("thank you sir for using me , have a nice day")
-            sys.exit() 
-         
-        elif "what are my events" in query or "events" in query :
-            import Quickstart 
+            sys.exit()
+
+        elif "what are my events" in query or "events" in query:
+            import Quickstart
             text = query
             service = Quickstart.authenticate_google()
-            Quickstart.get_events(Quickstart.get_date(text),service)  
-        
-while True:
-    query = take_command()
-    if "poor connection" == query:
-        speak(
-            "Sir,due to poor internet connection , i am not able to follow your commands, please connect to internet sir")
-        speak(
-            "sir,i will wait for 10 seconds, for you to connect to internet ")
-        time.sleep(10)
-        speak("ok sir i am done,i hope your are connected to internet")
-        query = take_command()
-        if "poor connection" == query:
-            speak(
-                "sir, i think you don't have proper intenet connection, so i am going offline")
-            sys.exit()
-    elif None == query:
-        pass
-    elif "hello jarvis" in query:
-        from features import wish
-        wish()
-        # query = take_command()
-        desire()
+            Quickstart.get_events(Quickstart.get_date(text), service)
+
+        elif "power" in query or "-" in query or "into" in query or "+" in query or "multiply" in query or "divided by" in query:
+            from features import calculator
+            speak("hang on sir, i am calculating")
+            calculator(query)
+
+        elif "calculate" in query or "find" in query or "calculation" in query:
+            speak("sir,please enter the calculation")
+            calci = input()
+            from features import input_calculator
+            input_calculator(calci)
+
+        elif "screenshot" in query:
+            from features import taking_screen_shot
+            taking_screen_shot()
+
+# while True:
+#     query = take_command()
+#     if "poor connection" == query:
+#         speak(
+#             "Sir,due to poor internet connection , i am not able to follow your commands, please connect to internet sir")
+#         speak(
+#             "sir,i will wait for 10 seconds, for you to connect to internet ")
+#         time.sleep(10)
+#         speak("ok sir i am done,i hope your are connected to internet")
+#         query = take_command()
+#         if "poor connection" == query:
+#             speak(
+#                 "sir, i think you don't have proper intenet connection, so i am going offline")
+#             sys.exit()
+#     elif None == query:
+#         pass
+#     elif "hello jarvis" in query:
+#         from features import wish
+#         wish()
+#         # query = take_command()
+#         desire()
+
+
+wish()
+desire()
